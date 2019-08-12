759dd69aafeb5518d7c56dcdd7adbea287964d60
Patrick Gordon <patrickgordon42@yahoo.com>
2019-08-12 18:11:31 -0500

    CartContext complete in ShoppingCart.js and Navigation.js

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function App() {
 		
         <Route exact path="/" component={Products} />
 
-        <Route path="/cart" render={() => <ShoppingCart />} />
+        <Route path="/cart" component={ShoppingCart} />
 		</CartContext.Provider>
       </ProductContext.Provider>
     </div>
